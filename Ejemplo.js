ceb20e33615fa602407400ac33810b6589fae618
marce0993 <marce_0993@hotmail.com>
2016-09-01 15:16:19 -0500

    Update Ejemplo.js

--- Ejemplo.js
+++ Ejemplo.js
@@ -1,4 +1,4 @@
 var miCanvas = document.getElementById("miCanvas");
-var contexto = miCanvas.getContext("2d");
+var contexto = miCanvas.getContext("3d");
 contexto.fillStyle="red";
 contexto.fillRect(30,30,50,50);
