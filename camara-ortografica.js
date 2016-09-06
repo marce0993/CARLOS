2c86f5c5bd35e6c34e936aa3659c17bde31af380
marce0993 <marce_0993@hotmail.com>
2016-09-06 15:21:18 -0500

    Update camara-ortografica.js

--- camara-ortografica.js
+++ camara-ortografica.js
@@ -1,4 +1,5 @@
 var camara = new THREE.OrtographicCamera();
+
 camara.left = window.innnerWidth / -2;
 camara.right = window.innerWidth / 2;
 camara.top = window.innerHeight /2;
@@ -8,3 +9,21 @@ camara.far = 1000;
 camara.updateProjectionMatrix();
 
 camara position.z = 100;
+
+var cubo = new THREE.Mesh(new THREE.BoxGeometry(30,30,30),
+                            new THREE.MeshNormalMaterial());
+esfera1.position.x= 50;
+
+var esfera2 = new THREE.Mesh(new THREE.SphereGeometry(15),
+                            new TRHEE.MeshNormalMaterial() );
+  esfera2.position.x =-50;
+  esfera2.position.z= -100;
+  
+  var escena = new THREE.Scene();
+  escena.add(esfera1);
+  escena.add(cubo);
+  var renderizador = new THREE.WebGLRenderer();
+  renderizador.setSize(window.innerWidth,
+                      window.innerHeight);
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena,camara);
