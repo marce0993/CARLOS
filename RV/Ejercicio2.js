9ceafcf0d368278c9992a6423923257111085e93
marce0993 <marce_0993@hotmail.com>
2016-09-08 16:06:36 -0500

    Update Ejercicio2.js

--- RV/Ejercicio2.js
+++ RV/Ejercicio2.js
@@ -5,7 +5,7 @@ var renderizador=new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
 var forma=new THREE.CylinderGeometry(0.5, 1, 2);
-var forma2=new THREE.SphereGeometry(1.5,8,6);
+var forma2=new THREE.SphereGeometry(1.5,30,15);
 var material=new THREE.MeshBasicMaterial({color: 0x704214});
 var material2=new THREE.MeshBasicMaterial({color: 0x23530f});
 var cilindro=new THREE.Mesh(forma,material);
