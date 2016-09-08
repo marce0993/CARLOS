6e098f6aef90112593abf9a5763e80f65482351c
marce0993 <marce_0993@hotmail.com>
2016-09-08 15:39:35 -0500

    Update Ejercicio2.js

--- RV/Ejercicio2.js
+++ RV/Ejercicio2.js
@@ -1,15 +1,4 @@
-var escena = new THREE.Scene();
-var camara = new THREE.PerspectiveCamera();
-camara.position.z = 5;
-var renderizador = new THREE.WebGLRenderer();
-renderizador.setSize( window.innerHeight*.95,
-                    	window.innerHeight*.95);
-			
-document.body.appendChild (renderizador.domElement);
-var forma = new THREE.BoxGeometry ( 1, 1, 1);
-var material = new THREE.MeshNormalMaterial(); 
-var cubo = new THREE.Mesh(forma, material);
-cubo.rotateX(-Math.PI/4); 
-cubo.rotateY(Math.PI/4);
-escena.add(cubo);
-renderizador.render(escena,camara);
+var miCanvas = document.getElementById("miCanvas");
+var contexto = miCanvas.getContext("2d");
+contexto.fillStyle="red";
+contexto.fillRect(30,30,50,50)
