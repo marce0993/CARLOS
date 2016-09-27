97214ea5a20b26cfcd9949393d7330678b977c2f
marce0993 <marce_0993@hotmail.com>
2016-09-27 15:50:15 -0500

    Update Ejemplotextura.js

--- RV/Ejemplotextura.js
+++ RV/Ejemplotextura.js
@@ -1,6 +1,5 @@
 var TEXTURA = new Object();
-
-TEXTURA.retrollamada = function(textura){
+TEXTURA.retrollamada = function(textura) {
   var material = new THREE.MeshBasicMaterial({map: textura});
    TEXTURA.malla = new THREE.Mesh(new THREE.SphereGeometry(1),
                                   material);
